361af069cd428a824510d6f2f72343732e924eea
mbask <marco@bascietto.name>
2015-06-21 17:15:20 +0200

    Prevent negative tank charge

--- R/tank_functions.R
+++ R/tank_functions.R
@@ -1,3 +1,4 @@
 get_tank_charge <- function(precipitation, evaporation_loss, precipitation_area) {
-  (precipitation - evaporation_loss) * precipitation_area
+  charge <- (precipitation - evaporation_loss) * precipitation_area
+  ifelse(charge > 0, charge, 0)
 }
