7a893c8459d979704ce0c94a633c806c3b8ea4f0
mbask <marco@bascietto.name>
2016-04-19 15:18:23 +0200

    New discharge_coeff replaces evaporation_loss argument

--- R/tank_functions.R
+++ R/tank_functions.R
@@ -4,34 +4,35 @@
 #'
 #' @param precipitation the amount of rain fallen in mm
 #' @param precipitation_area the surface area of the roof
-#' @param evaporation_loss mm of rain lost to evaporation on the roof (optional, defaults to 0)
+#' @param discharge_coef The discharge coefficient determines the proportion of rainfall to the total rainfall, which is actually discharged from the roof (optional, defaults to 0.8, for an inclined hard roof, with shingles)
 #'
 #' @return volume of water collected (numeric, in l)
 #' @export
 #'
 #' @examples
-#' # Amount of tank charge following a 10 mm rain on a 150 m2 roof, no evaporation
+#' # Amount of tank charge following a 10 mm rain on a 150 m2 roof
 #' get_tank_charge(10, 150)
-#' # Amount of tank charge following a 10 mm rain on a 150 m2 roof,
-#' # assuming 2 mm of rain evaporated on hot roof
-#' get_tank_charge(10, 150, 2)
+#' # Amount of tank charge following a 10 mm rain on a 150 m2 green roof,
+#' get_tank_charge(10, 150, 0.3)
 get_tank_charge <- function(precipitation, 
                             precipitation_area, 
-                            evaporation_loss = 0) {
+                            discharge_coef = 0.8) {
   assert_that(is.numeric(precipitation))
   assert_that(is.numeric(precipitation_area))
-  assert_that(is.numeric(evaporation_loss))
-
+  assert_that(is.numeric(discharge_coef))
+  assert_that(discharge_coef > 0)
+  assert_that(discharge_coef <= 1)
+  
   if (sum(precipitation >= 0) != length(precipitation)) {
     warning("Negative precipitation")
   }
   if (sum(precipitation_area > 0) != length(precipitation_area)) {
     warning("Zero or negative precipitation_area")
   }
-  if (sum(evaporation_loss >= 0) != length(evaporation_loss)) {
-    warning("Negative evaporation_loss")
-  }
+#   if (sum(evaporation_loss >= 0) != length(evaporation_loss)) {
+#     warning("Negative evaporation_loss")
+#   }
 
-  charge <- (precipitation - evaporation_loss) * precipitation_area
+  charge <- precipitation * discharge_coef * precipitation_area
   ifelse(charge > 0, charge, 0)
 }
