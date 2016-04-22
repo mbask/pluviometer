7fdf75b921a7d7b2f2475bbf51d71bc4a25c0c86
mbask <marco@bascietto.name>
2016-04-22 07:46:39 +0200

    Added get_tank_charge_on_same_surface function closure

--- R/tank_functions.R
+++ R/tank_functions.R
@@ -32,3 +32,27 @@ get_tank_charge <- function(precipitation,
 
   precipitation * discharge_coef * precipitation_area
 }
+
+#' Estimate the amount of rain flown into a tank collected for a given surface and discharge coefficient
+#'
+#' Simple wrapper around \code{get_tank_charge}
+#'
+#' @param precipitation_area the surface area of the roof
+#' @param discharge_coef the discharge coefficient determines the proportion of rainfall to the total rainfall, which is actually discharged from the roof (optional, defaults to 0.8, for an inclined hard roof, with shingles)
+#'
+#' @return \code{get_tank_charge} function with arguments \code{precipitation_area} and \code{discharge_coeff} filled
+#' @export
+#'
+#' @examples
+#' rainfall_in_tank <- get_tank_charge_on_same_surface(121, 0.8)
+#' rainfall_in_tank(1)
+#' rainfall_in_tank(100)
+get_tank_charge_on_same_surface <- function(precipitation_area, discharge_coef) {
+  
+  function(precipitation) {
+    get_tank_charge(
+      precipitation,
+      precipitation_area, 
+      discharge_coef)
+  }
+}
